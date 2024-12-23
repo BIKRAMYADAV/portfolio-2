27f7979c7cc7377a0e5f7d9e00c80327d072764e
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2024-12-24 00:19:01 +0530

    feat: set up the backend server

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT
+const app = express()
+
+
+app.listen(PORT, () => {
+    console.log('The app is listening on port: ',PORT);
+})
